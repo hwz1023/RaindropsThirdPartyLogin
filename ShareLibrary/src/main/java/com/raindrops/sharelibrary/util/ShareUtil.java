f7d9e7c0e8b0e951685f3ac5825447b31dd83229
hwz <441679925@qq.com>
2017-05-17 15:25:30 +0800

    优化

--- ShareLibrary/src/main/java/com/raindrops/sharelibrary/util/ShareUtil.java
+++ ShareLibrary/src/main/java/com/raindrops/sharelibrary/util/ShareUtil.java
@@ -101,7 +101,7 @@ public class ShareUtil {
         Glide.with(mContext)
                 .load(imageUrl).asBitmap()
                 .toBytes()
-                .into(new SimpleTarget<byte[]>(120, 120) {
+                .into(new SimpleTarget<byte[]>(90, 90) {
                     public void onLoadFailed(Exception e, Drawable errorDrawable) {
                         super.onLoadFailed(e, errorDrawable);
                         if (ShareConfig.isShareCallBack())
@@ -151,7 +151,7 @@ public class ShareUtil {
         Glide.with(mContext)
                 .load(imageUrl).asBitmap()
                 .toBytes()
-                .into(new SimpleTarget<byte[]>(120, 120) {
+                .into(new SimpleTarget<byte[]>(90, 90) {
                     @Override
                     public void onLoadFailed(Exception e, Drawable errorDrawable) {
                         super.onLoadFailed(e, errorDrawable);
