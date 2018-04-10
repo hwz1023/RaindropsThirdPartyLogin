6f8bbbd1b3d51a661868357061a9be0ad440f679
hwz <441679925@qq.com>
2018-04-11 00:21:12 +0800

    支付

--- ShareLibrary/src/main/java/com/raindrops/sharelibrary/WXEntryActivity.java
+++ ShareLibrary/src/main/java/com/raindrops/sharelibrary/WXEntryActivity.java
@@ -8,8 +8,6 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.raindrops.sharelibrary.util.LoginUtil;
-import com.raindrops.sharelibrary.util.ThirdPartyPayUtils;
-import com.tencent.mm.opensdk.constants.ConstantsAPI;
 import com.tencent.mm.opensdk.modelbase.BaseReq;
 import com.tencent.mm.opensdk.modelbase.BaseResp;
 import com.tencent.mm.opensdk.modelmsg.SendAuth;
@@ -37,76 +35,63 @@ public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
 
     @Override
     public void onReq(BaseReq baseReq) {
+        Log.e("onResp", "type" + baseReq.getType());
     }
 
     @Override
     public void onResp(BaseResp baseResp) {
         String msg = "";
-        if (baseResp.getType() == ConstantsAPI.COMMAND_PAY_BY_WX) {
-            if (baseResp.errCode == BaseResp.ErrCode.ERR_OK) {
-                ThirdPartyPayUtils.getInstance().getCallback().success();
-            } else if (baseResp.errCode == BaseResp.ErrCode.ERR_USER_CANCEL) {
-                Toast.makeText(this, "取消支付", Toast.LENGTH_SHORT).
-                        show();
-            } else {
-                Toast.makeText(this, "支付失败", Toast.LENGTH_SHORT).
-                        show();
-                ThirdPartyPayUtils.getInstance().getCallback().error(String.valueOf(baseResp.errCode), baseResp.errStr);
-            }
-        } else {
-            switch (baseResp.errCode) {
-                case BaseResp.ErrCode.ERR_OK:
-                    //			result = R.string.errcode_success
-                    if (baseResp instanceof SendAuth.Resp) {
-                        SendAuth.Resp resp = (SendAuth.Resp) baseResp;
-                        if (ShareConfig.getInstance()
-                                .getThirdPartyType() == ShareIntentStaticCode
-                                .THIDR_PARTY_LOGIN) {
-                            LoginUtil.getAccessToken(resp.code);
-                        }
-                        if (ShareConfig.getInstance()
-                                .getThirdPartyType() == ShareIntentStaticCode
-                                .THIDR_PARTY_AUTH) {
-                            ShareConfig.getInstance().getAuthCallBack().onComplete(resp.code);
-                        }
-                    } else {
-                        if (ShareConfig.isShareCallBack())
-                            ShareConfig.getInstance().getShareCallBack().shareSuccess();
+        switch (baseResp.errCode) {
+            case BaseResp.ErrCode.ERR_OK:
+                //			result = R.string.errcode_success
+                if (baseResp instanceof SendAuth.Resp) {
+                    SendAuth.Resp resp = (SendAuth.Resp) baseResp;
+                    if (ShareConfig.getInstance()
+                            .getThirdPartyType() == ShareIntentStaticCode
+                            .THIDR_PARTY_LOGIN) {
+                        LoginUtil.getAccessToken(resp.code);
                     }
-                    break;
-                case BaseResp.ErrCode.ERR_USER_CANCEL://用户取消
-                    msg = "用户取消授权";
-                    break;
-                case BaseResp.ErrCode.ERR_AUTH_DENIED:
-                    msg = "用户拒绝授权";
-                    break;
-                default:
-                    msg = "授权失败";
-                    break;
-            }
-            Log.e("msg", msg);
-            if (msg.length() > 0) {
-
-
-                if (ShareConfig.getInstance().getThirdPartyType() == ShareIntentStaticCode
-                        .THIDR_PARTY_AUTH) {
-                    ShareConfig.getInstance().getAuthCallBack().onError(-100,
-                            ShareIntentStaticCode
-                                    .THIDR_PARTY_WECHAT, msg);
-                } else if (ShareConfig.getInstance().getThirdPartyType() == ShareIntentStaticCode
-                        .THIDR_PARTY_LOGIN) {
-                    Toast.makeText(this, msg, Toast.LENGTH_SHORT).
-                            show();
-                    ShareConfig.getInstance().getLoginCallBack().onError(-100,
-                            ShareIntentStaticCode
-                                    .THIDR_PARTY_WECHAT, msg);
+                    if (ShareConfig.getInstance()
+                            .getThirdPartyType() == ShareIntentStaticCode
+                            .THIDR_PARTY_AUTH) {
+                        ShareConfig.getInstance().getAuthCallBack().onComplete(resp.code);
+                    }
                 } else {
                     if (ShareConfig.isShareCallBack())
-                        ShareConfig.getInstance().getShareCallBack().shareError();
+                        ShareConfig.getInstance().getShareCallBack().shareSuccess();
                 }
+                break;
+            case BaseResp.ErrCode.ERR_USER_CANCEL://用户取消
+                msg = "用户取消授权";
+                break;
+            case BaseResp.ErrCode.ERR_AUTH_DENIED:
+                msg = "用户拒绝授权";
+                break;
+            default:
+                msg = "授权失败";
+                break;
+        }
+        Log.e("msg", msg);
+        if (msg.length() > 0) {
+
+
+            if (ShareConfig.getInstance().getThirdPartyType() == ShareIntentStaticCode
+                    .THIDR_PARTY_AUTH) {
+                ShareConfig.getInstance().getAuthCallBack().onError(-100,
+                        ShareIntentStaticCode
+                                .THIDR_PARTY_WECHAT, msg);
+            } else if (ShareConfig.getInstance().getThirdPartyType() == ShareIntentStaticCode
+                    .THIDR_PARTY_LOGIN) {
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).
+                        show();
+                ShareConfig.getInstance().getLoginCallBack().onError(-100,
+                        ShareIntentStaticCode
+                                .THIDR_PARTY_WECHAT, msg);
+            } else {
+                if (ShareConfig.isShareCallBack())
+                    ShareConfig.getInstance().getShareCallBack().shareError();
             }
         }
-
         finish();
     }
 
